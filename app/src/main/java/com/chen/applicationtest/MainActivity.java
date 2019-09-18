be6b6e666fbdae183adfda9b49f2f692ebed6910
chenhanlin2333 <chenhanlin2333@163.com>
2019-09-18 22:28:36 +0800

    master1

--- app/src/main/java/com/chen/applicationtest/MainActivity.java
+++ app/src/main/java/com/chen/applicationtest/MainActivity.java
@@ -3,6 +3,7 @@ package com.chen.applicationtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "1", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "2", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "3", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        int i = 1;
     }
 }
